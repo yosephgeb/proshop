bd65fb88ef33a67bcddcae21eae1f0fa4d395393
yosephog <thorfry@yahoo.com>
2024-04-16 16:59:54 +0200

    frontend screens, description footer and headers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import {Outlet} from 'react-router-dom'
 const App = () => {
   return (
-    <h1>Welocke To Proshop</h1>
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Outlet />
+        </Container>
+        
+      </main>
+      <Footer />
+    </>
+    
   )
 }
 

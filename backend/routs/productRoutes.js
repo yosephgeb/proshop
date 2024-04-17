354ba8a17486f7daeffaa218ab39b10421848a01
yosephog <thorfry@yahoo.com>
2024-04-17 15:29:07 +0200

    db, models seeding

---
+++ backend/routs/productRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { getProducts, getProductsById } from '../controllers/productController.js'
+
+const router = express.Router()
+
+router.route('/').get(getProducts)
+router.route('/:id').get(getProductsById)
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,20 @@
-import express from 'express'
-import products from './data/products.js'
+import express, { application } from 'express'
+import productRoutes from './routs/productRoutes.js'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 const port = process.env.PORT || 5000
-
+connectDB()
 const app = express()
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
-    res.json(product);
+application.get('/', (reg, res) => {
+    res.send('API is running.....')
 })
+app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
+
 app.listen(port, () => console.log(`Server running on port ${port}`))

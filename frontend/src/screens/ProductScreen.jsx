89230f68034c4a193d2f716b5864c165faba7abb
yosephog <thorfry@yahoo.com>
2024-04-17 15:27:22 +0200

    backend ,db connection

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
-import products from '../products';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap'
 import Rating from '../components/Rating';
+import axios from 'axios';
 
 export const ProductScreen = () => {
-    const { id:productId } = useParams();
-    const product = products.find((p) => p._id === productId)
+    const[product, setProduct] = useState({})
+    const{ id: productId} = useParams();
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const {data} = await axios.get(`/api/products/${productId}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [productId])
   return (
     <>
         <Link className="btn btn-light my-3" to='/'>
